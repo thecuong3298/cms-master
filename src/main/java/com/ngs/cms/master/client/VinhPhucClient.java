0f593c6862def5e1c03f71ab263e58461a282efd
cuongdt <cuongdt@ngscds.vn>
2022-07-21 10:55:11 +0700

    add jpa

--- src/main/java/com/ngs/cms/master/client/VinhPhucClient.java
+++ src/main/java/com/ngs/cms/master/client/VinhPhucClient.java
@@ -3,11 +3,16 @@ package com.ngs.cms.master.client;
 
 import com.common.dto.ResponseWrapper;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
 
 @FeignClient(name = "vinh-phuc", url = "${ngs.clients.vinh-phuc.url}")
 public interface VinhPhucClient {
 
-  @GetMapping("posts-quantity-report")
-  ResponseWrapper test();
+//  @GetMapping("posts-quantity-report")
+  @GetMapping("partner/test")
+  ResponseWrapper test(@RequestParam Date fromDate, @RequestParam Date toDate, @RequestParam Pageable pageable);
 }
